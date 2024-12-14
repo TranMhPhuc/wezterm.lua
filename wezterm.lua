2cc8b2c00baeac8236fec4314d8109799baebbc0
TranMhPhuc <49608103+TranMhPhuc@users.noreply.github.com>
2024-12-14 22:13:10 +0700

    Update wezterm.lua: Fix MacOS shortcut, change font

--- wezterm.lua
+++ wezterm.lua
@@ -12,12 +12,18 @@ end
 
 -- This is where you actually apply your config choices
 
+config.font = wezterm.font {
+    family = 'JetBrains Mono',
+    weight = 'Medium'
+}
+
 -- Changing the color scheme:
 -- config.color_scheme = 'One Dark (Gogh)'
+-- config.color_scheme = 'Monokai (terminal.sexy)'
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 
 -- Opacity for window background
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.92
 
 -- Window padding
 local horizontal_padding = 16
@@ -30,7 +36,7 @@ config.window_padding = {
 }
 
 -- Width & height
-config.initial_cols = 100
+config.initial_cols = 140
 config.initial_rows = 35
 
 -- Set title buttons position
@@ -38,6 +44,18 @@ config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.integrated_title_button_style = "Windows"
 config.integrated_title_buttons = { 'Close', 'Hide', 'Maximize'}
 config.integrated_title_button_alignment = "Left"
+
+-- For macOS: Option-Left/Right
+
+config.keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    { key = 'LeftArrow', mods = 'OPT', action = wezterm.action.SendString '\x1bb' },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    { key = 'RightArrow', mods = 'OPT', action = wezterm.action.SendString '\x1bf' },
+}
+
+-- Show scroll bar
+config.enable_scroll_bar = true
 
 -- Show confirm dialog when exiting 
 config.window_close_confirmation = 'AlwaysPrompt'
